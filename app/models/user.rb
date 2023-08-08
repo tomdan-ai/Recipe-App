de2a617d851e3997e454d7184e7517b30a56bd4f
Tom Daniel <tomudoh258@gmail.com>
2023-08-08 11:03:10 +0100

    Modify User model to enable authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 # Represents a food item that can be used in recipes.
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :recipes
   has_many :foods
 end
