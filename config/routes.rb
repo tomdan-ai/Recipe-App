0ace9301463636eeb78f16401e5873cc331ba5bb
Tom Daniel <tomudoh258@gmail.com>
2023-08-08 11:06:08 +0100

    Configure routes for Devise and user views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :users, only: [:show, :edit, :update]
+
+  # Add the following lines to create routes for viewing recipe details and deleting recipes
+  resources :recipes, only: [:show, :destroy, :index]
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
