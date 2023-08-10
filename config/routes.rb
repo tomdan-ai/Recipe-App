fae52be8b2aeb1485c463654bfcd064a18bcb5f6
Adams-Ghub <bamfoadamsf@gmail.com>
2023-08-10 18:20:06 +0000

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,22 @@ Rails.application.routes.draw do
 
   resources :foods, only: %i[index new create destroy]
 
-  resources :recipes, only: %i[index show destroy new create]
+  resources :recipes, only: %i[index show destroy new create] do
+    resources :recipe_foods, only: %i[new create]
+    
+  end
+  resources :general_shopping_lists, only: %i[index]
 
   # Add the following line to create a route for displaying public recipes
   get 'public_recipe', to: 'recipes#public_recipe', as: :public_recipe_recipes
+
+  post 'recipes/:id/toggle', to: 'recipes#toggle', as: :public_recipe_recipes
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+
