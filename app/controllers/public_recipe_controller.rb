dcf7dbb1d27d28907958bd40f9254bec63e56127
Tom Daniel <tomudoh258@gmail.com>
2023-08-11 14:14:32 +0100

    Secure public Recipes from N+1 Error

--- app/controllers/public_recipe_controller.rb
+++ app/controllers/public_recipe_controller.rb
@@ -2,6 +2,6 @@ class PublicRecipesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @recipes = Recipe.where(public: true).order(created_at: :desc)
+    @recipes = Recipe.where(public: true).order(created_at: :desc).includes(:recipe_foods)
   end
 end
