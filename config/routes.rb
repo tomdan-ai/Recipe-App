5b54f0095a951bf13fec39d591004b8ff6cc6bae
Adams-Ghub <bamfoadamsf@gmail.com>
2023-08-12 00:51:40 +0000

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
   resources :foods, only: %i[index new create destroy]
 
   resources :recipes, only: %i[index show destroy new create] do
-    # member do
-    #   patch '/public_recipes/:id', to: 'public_recipes#update', as: :update_public_recipe
-    # end
     patch 'toggle_public', on: :member
     resources :recipe_foods, only: %i[new create destroy]
   end
@@ -16,6 +13,7 @@ Rails.application.routes.draw do
   # Add the following line to create a route for displaying public recipes
   get 'public_recipe', to: 'recipes#public_recipe', as: :public_recipe_recipes
 
+  root 'recipes#public_recipe'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
