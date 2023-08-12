64e3464a1e7af3097a11c9074b240c783d2198ae
Tom Daniel <tomudoh258@gmail.com>
2023-08-12 10:05:34 +0100

    Set Preparation time to Hours

--- db/migrate/20230807145014_create_recipes.rb
+++ db/migrate/20230807145014_create_recipes.rb
@@ -3,7 +3,7 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.datetime :prescription_time
+      t.integer :prescription_time
       t.integer :cooking_time
       t.text :description
       t.boolean :public
