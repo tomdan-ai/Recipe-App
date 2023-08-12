3ace5d90188870f5fcca8b411173485232e3ad9d
Adams-Ghub <bamfoadamsf@gmail.com>
2023-08-12 00:50:11 +0000

    Create integration test

---
+++ spec/integration/food_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'foods/index', type: :view do
+  it 'displays a link to add a new food' do
+  end
+
+  it 'displays a message when no foods' do
+  end
+
+  it 'displays the foods in a table' do
+  end
+
+  it 'renders the food partial for each food' do
+  end
+end
